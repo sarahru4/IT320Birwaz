cd24a65f3291354dcbe21ce234311c93cb6348c8
sarahru4 <444200758@student.ksu.edu.sa>
2025-11-24 05:21:57 +0300

    Add files via upload

---
+++ Html_Pages/db_config.php
@@ -0,0 +1,20 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root'); 
+define('DB_NAME', 'birwaz'); 
+function getDBConnection() {
+    
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 8889);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $conn->set_charset("utf8mb4");
+    return $conn;
+}
+
+
+
